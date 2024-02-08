6cacbea6df119d6ba401979239fe0a8e8b1fd843
katrina-macadams <145619617+katrina-macadams@users.noreply.github.com>
2024-02-07 19:34:43 -0500

    Added dragging to event listener
    added an eventlistener when someone starts dragging puzzle piece

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-let theButtons = document.querySelectorAll('#buttonHolder img')
-    puzzleBoard = document.querySelector('.puzzle-board');
+let theButtons = document.querySelectorAll('#buttonHolder img'),
+    puzzleBoard = document.querySelector('.puzzle-board'),
+    puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+    draggedPieces;
 
     function changeBGImage() {
         puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg`;
     }
 
+    function handleStartDrag() {
+        console.log('started dragging this piece', this);
+        draggedPiece = this;
+    }
+
     theButtons.forEach(button => button.addEventListener('click', changeBGImage));
+    puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handleStartDrag));
